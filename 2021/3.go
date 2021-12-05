7b2c08fdb9a20cecb738ab1b7f0230f1c00b2dab
Blynxy Furyfur <blynxy@cathead.media>
2021-12-05 02:16:34 -0600

    finished problem 3

--- 2021/3.go
+++ 2021/3.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 	"strconv"
 )
-var bitlength = 12
+var bitlength = 5
 
 func main() {
 	contents := FileByLine("3.txt")
+	//contents := FileByLine("3_sample.txt")
+	bitlength = len(contents[0])
+	fmt.Println("length: ", bitlength)
 
 	var data []int
 	for _, val := range contents {
-		n,_ := strconv.ParseInt(val,2, 64)
-		fmt.Printf("%05s\n", strconv.FormatInt(n,2))
+		n,err := strconv.ParseInt(val,2, 64)
+		if err != nil {
+			fmt.Println("DEAD")
+		}
+		//fmt.Printf("%05s\n", strconv.FormatInt(n,2))
 		data = append(data, int(n))
 	}
+	fmt.Println(len(contents), " " , len(data))
 
 
 	lcb, mcb := FindLcbAndMcb(data)
@@ -29,26 +36,39 @@ func FindLcbAndMcb(d []int) (int, int) {
 	} else {
 		lcb, mcb = z,o
 	}
+	fmt.Printf("z/o: [%d][%d]\n",len(z),len(o))
+	fmt.Printf("l/m: [%d][%d]\n",len(lcb),len(mcb))
 	// lcb
 	for i:=1;i<bitlength;i++ {
 		z,o = SplitOnPostion(lcb, i)
+		fmt.Printf("z/o: [%d][%d]\n",len(z),len(o))
 		if len(z) > len(lcb)/2 {
 			lcb = o
+			fmt.Printf("     (o)\n")
 		} else {
 			lcb = z
+			fmt.Printf("     (z)\n")
 		}
+
+		fmt.Printf("l: [%d]\n",len(lcb))
 		if len(lcb) == 1 {
 			break
 		}
 	}
+	println("end lcb - - - - - -")
 	// mcb
 	for i:=1;i<bitlength;i++ {
 		z,o = SplitOnPostion(mcb, i)
-		if len(o) > len(mcb)/2 || len(mcb) == 2{
-			mcb = o
-		} else {
+		fmt.Printf("z/o: [%d][%d]\n",len(z),len(o))
+		if len(z) > len(mcb)/2 {
 			mcb = z
+			fmt.Printf("     (z)\n")
+		} else {
+			mcb = o
+			fmt.Printf("     (o)\n")
 		}
+
+		fmt.Printf("l: [%d]\n",len(mcb))
 		if len(mcb) == 1 {
 			break
 		}
@@ -57,7 +77,7 @@ func FindLcbAndMcb(d []int) (int, int) {
 	return lcb[0],mcb[0]
 }
 func SplitOnPostion(d []int, pos int) ([]int, []int) {
-	fmt.Println("NEW SPLITTING ____")
+	//fmt.Println("NEW SPLITTING ____")
 	var mask int
 	mask = 1 << (bitlength-1-pos)
 	var ones,zeros []int
@@ -68,13 +88,13 @@ func SplitOnPostion(d []int, pos int) ([]int, []int) {
 			zeros = append(zeros, v)
 		}
 	}
-	fmt.Printf("mask: %012s\n", strconv.FormatInt(int64(mask), 2))
-	for _,v := range ones {
-		fmt.Printf("o---: %012s [%d]\n", strconv.FormatInt(int64(v), 2),v)
-	}
-	fmt.Printf("mask: %012s\n", strconv.FormatInt(int64(mask), 2))
-	for _,v := range zeros {
-		fmt.Printf("z---: %012s [%d]\n", strconv.FormatInt(int64(v), 2),v)
-	}
+	//fmt.Printf("mask: %012s\n", strconv.FormatInt(int64(mask), 2))
+	//for _,v := range ones {
+	//	fmt.Printf("o---: %012s [%d]\n", strconv.FormatInt(int64(v), 2),v)
+	//}
+	//fmt.Printf("mask: %012s\n", strconv.FormatInt(int64(mask), 2))
+	//for _,v := range zeros {
+	//	fmt.Printf("z---: %012s [%d]\n", strconv.FormatInt(int64(v), 2),v)
+	//}
 	return zeros, ones
 }
